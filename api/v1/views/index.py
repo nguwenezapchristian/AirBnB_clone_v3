c836c1e78cb21837ba1bf02698e56552f9506ceb
Nguweneza P Christian <nguwenezacc982@gmail.com>
2024-02-24 22:49:22 +0200

    retrieves the number of each objects by type

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -10,3 +10,24 @@ def status():
     return jsonify({
         "status": "OK"
         })
+
+@app_views.route("/stats", methods=["GET"], strict_slashes=False)
+def Somestats():
+    """Retrieves the number of objects per each type"""
+    from models.amenity import Amenity
+    from models.city import City
+    from models.place import Place
+    from models.user import User
+    from models.review import Review
+    from models.state import State
+    from models import storage
+    import json
+    obj = {
+        "amenities": storage.count(Amenity),
+        "cities": storage.count(City),
+        "places": storage.count(Place),
+        "reviews": storage.count(Review),
+        "states": storage.count(State),
+        "users": storage.count(User)
+    }
+    return json.dumps(obj, indent=2)
