8d23482861031a99e50b96677324ef8c6734e352
elyton250 <nelysemathieu@gmail.com>
2024-02-25 17:19:44 +0200

    error 404 handler

--- api/v1/app.py
+++ api/v1/app.py
@@ -2,7 +2,7 @@
 """This is the Flask module for our API."""
 
 
-from flask import Flask
+from flask import Flask, jsonify
 from models import storage
 from api.v1.views import app_views
 from os import getenv
@@ -26,6 +26,12 @@ def close_app(exception):
         exception (_type_): _description_
     """
     storage.close()
+
+
+@app.errorhandler(404)
+def not_found(error):
+    """this handles page not found error"""
+    return jsonify({"error": "Not found"}), 404
 
 
 if __name__ == "__main__":
